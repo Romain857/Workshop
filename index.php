77438f596d8f8de7b2db51ddea1a06a96da06cf5
gabriel <gabriel.filiot@epsi.fr>
2020-09-08 11:34:42 +0200

    update: Mise en place d'une base

--- index.php
+++ index.php
@@ -1,73 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="style.css">
-    <title>Workshop</title>
-    
-</head>
-<body>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">GeoDep</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Catégorie
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Départements</a>
-          <a class="dropdown-item" href="#">Sociétés</a>         
-        </div>
-      </li>
-        <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">deconnexion</a>
-      </li>
-    </ul>
+<?php require_once 'layout/header.php'; ?>
 
 
+    <main>
 
+        <?php use League\Csv\Reader;
 
+        //load the CSV document from a file path
+        $csv = Reader::createFromPath('donnees_gaz_2017.csv', 'r');
+        $csv->setHeaderOffset(0);
 
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
-    
+        $header = $csv->getHeader(); //returns the CSV header record
+        $records = $csv->getRecords(); //returns all the CSV records as an Iterator object
 
-    <?php 
-        require_once 'vendor/autoload.php';
+        //    var_dump($header);
+        /*
+        foreach ($records as $ligne){
+            var_dump($ligne);
 
+        };  */ ?>
 
+    </main>
 
-        use League\Csv\Reader;
-
-//load the CSV document from a file path
-$csv = Reader::createFromPath('donnees_gaz_2017.csv', 'r');
-$csv->setHeaderOffset(0);
-
-$header = $csv->getHeader(); //returns the CSV header record
-$records = $csv->getRecords(); //returns all the CSV records as an Iterator object
-
-//    var_dump($header);
-/*
-foreach ($records as $ligne){
-    var_dump($ligne);
-
-};
-*/
-
-    ?>
-</body>
-</html>
+<?php require_once 'layout/footer.php'; ?>
