e3bfea0c8f7fb41e5347ba37a6ee36e5a0a4d7e4
Romain857 <romainmart.85@gmail.com>
2020-09-10 23:48:59 +0200

    echelle

--- carte.php
+++ carte.php
@@ -3,9 +3,15 @@
 <main>
 
 <div id="tip"> </div>
+<div class="pageMap">
 <div class="carte" id="cmap"></div>
 <script type="text/javascript" src="cmap/France.js"></script>
 <script type="text/javascript" src="cmap/cmap.js"></script>
+
+<div class="echelle">
+<img src="dist/images/echelle4.png">
+</div>
+</div>
 
 </main>
 
